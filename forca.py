782efdb7209beebe9ecd205a3a21bcf67e8fbe96
FelipeDaSilvaMachado <felipe.silvamachado@yahoo.com>
2024-11-15 09:44:00 -0300

    Atualizacoes 2.0

--- forca.py
+++ forca.py
@@ -1,13 +1,16 @@
 import tkinter as tk
 import random
 
+
 def escolher_palavra():
     palavras = ["MITS", "altair", "programacao", "etec", "tecnologia",
                 "computador", "Ridley", "Microsoft", "Apple", "informatica"]
     return random.choice(palavras).upper()
 
+
 def atualizar_palavra():
     palavra_label.config(text=" ".join(letras_corretas))
+
 
 def desenhar_boneco():
     # Desenhar partes do boneco com base no número de tentativas restantes
@@ -27,6 +30,7 @@ def desenhar_boneco():
         canvas.create_line(117, 140, 222, 180)
         # Criando o segundo linha do X da forca
         canvas.create_line(117, 180, 222, 140)
+
 
 def verificar_letra():
     global tentativas
@@ -62,6 +66,47 @@ def verificar_letra():
             text=f"Você perdeu! A palavra era: {palavra_secreta}")
         letra_entry.config(state=tk.DISABLED)
 
+# Esta linha define uma nova função chamada reiniciaJogo.
+# Esta função será chamada quando o botão "Jogar Novamente" for clicado.
+
+
+def reiniciaJogo():
+    # Essa linha indica que as variáveis palavra_secreta, letras_corretas,
+    # tentativas e letras_erradas serão usadas dentro da função reiniciaJogo,
+    # e estas variáveis são globais, o que significa que são definidas fora
+    # da função.
+    global palavra_secreta, letras_corretas, tentativas, letras_erradas
+    # Esta linha redefine palavra_secreta escolhendo uma nova palavra secreta
+    # para o jogo.
+    palavra_secreta = escolher_palavra()
+    # Esta linha redefine letras_corretas para uma lista de sublinhados,
+    # representando a nova palavra secreta, onde cada caractere é representado
+    # por um sublinhado.
+    letras_corretas = ["_" for _ in palavra_secreta]
+    # Esta linha redefine o número de tentativas restantes para 6,
+    # reiniciando o contador de tentativas.
+    tentativas = 6
+    # Esta linha redefine a lista letras_erradas para uma lista vazia,
+    # removendo quaisquer tentativas erradas anteriores.
+    letras_erradas = []
+    # Esta linha chama a função atualizar_palavra para atualizar a exibição
+    # da palavra na interface com os sublinhados da nova palavra.
+    atualizar_palavra()
+    # Esta linha limpa o canvas, removendo todos os desenhos anteriores
+    # (como partes do boneco).
+    canvas.delete("all")
+    # Essas linhas redesenham a estrutura da forca no canvas.
+    canvas.create_line(5, 400, 5, 50)
+    canvas.create_line(5, 50, 170, 50)
+    canvas.create_line(170, 50, 170, 100)
+    # Esta linha redefine o texto do resultado_label para uma string vazia,
+    # removendo qualquer mensagem anterior.
+    resultado_label.config(text="")
+    # Esta linha reativa a entrada de texto letra_entry, permitindo que o
+    # jogador insira novas letras.
+    letra_entry.config(state=tk.NORMAL)
+
+
 # Inicializa o jogo
 palavra_secreta = escolher_palavra()
 letras_corretas = ["_" for _ in palavra_secreta]
@@ -79,7 +124,7 @@ altura_tela = root.winfo_screenheight()
 
 # Define as dimensões da janela
 largura_janela = 500
-altura_janela = 615
+altura_janela = 623
 
 # Calcula as coordenadas x e y para centralizar a janela
 pos_x = (largura_tela - largura_janela) // 2
@@ -88,26 +133,61 @@ pos_y = (altura_tela - altura_janela) // 2
 # Define a geometria da janela
 root.geometry(f'{largura_janela}x{altura_janela}+{pos_x}+{pos_y}')
 
+# Essas linhas desenham a estrutura da forca no canvas.
 canvas = tk.Canvas(root, width=300, height=400)
 canvas.pack()
 canvas.create_line(5, 400, 5, 50)
 canvas.create_line(5, 50, 170, 50)
 canvas.create_line(170, 50, 170, 100)
 
+# Esta linha cria um widget Label na interface gráfica. Um
+# Label é usado para exibir texto ou imagens na interface.
 palavra_label = tk.Label(root, text=" ".join(
     letras_corretas), font=("Helvetica", 18))
+# Esta linha adiciona o Label à janela principal utilizando o
+# método pack, que é um dos métodos de gerenciamento de layout em Tkinter.
 palavra_label.pack(pady=20)
 
+# Esta linha cria um widget Entry na interface gráfica. Um Entry é um
+# campo de entrada de texto que permite ao usuário digitar uma linha de texto.
 letra_entry = tk.Entry(root, font=("Helvetica", 16))
+# Esta linha adiciona o Entry à janela principal utilizando o método pack,
+# que é um dos métodos de gerenciamento de layout em Tkinter.
 letra_entry.pack()
 
 # Vincula a tecla Enter para acionar o botão "Verificar"
 letra_entry.bind("<Return>", lambda event: verificar_letra())
+# Frame para os botões
+# Essa linha cria um novo contêiner Frame dentro da janela
+# principal root. Um Frame é um widget que pode conter outros widgets,
+# permitindo organizar o layout da janela.
+button_frame = tk.Frame(root)
+# Essa linha adiciona o Frame à janela principal e aplica um
+# preenchimento vertical (pady) de 15 pixels ao redor do Frame,
+# criando algum espaço vertical extra.
+button_frame.pack(pady=15)
+# Esta linha cria um botão chamado "Verificar" dentro do button_frame.
+# O texto do botão é "Verificar" e, quando clicado, ele executará a
+# função verificar_letra.
+verificar_button = tk.Button(
+    button_frame, text="Verificar", command=verificar_letra)
+# Essa linha empacota (adiciona) o botão "Verificar" ao button_frame
+# e o centraliza horizontalmente (side=tk.RIGHT).
+verificar_button.pack(side=tk.LEFT)
+# Esta linha cria um segundo botão chamado "Jogar Novamente" dentro
+# do button_frame. O texto do botão é "Jogar Novamente" e, quando clicado,
+# ele executará a função reiniciaJogo.
+jogarDeNovo = tk.Button(
+    button_frame, text="Jogar Novamente", command=reiniciaJogo)
+# Essa linha empacota (adiciona) o botão "Jogar Novamente" ao button_frame
+# e o centraliza horizontalmente (side=tk.CENTER).
+jogarDeNovo.pack(side=tk.RIGHT)
 
-verificar_button = tk.Button(root, text="Verificar", command=verificar_letra)
-verificar_button.pack(pady=10)
-
+# Esta linha cria um widget Label na interface gráfica. Um Label é usado
+# para exibir texto ou imagens na interface.
 resultado_label = tk.Label(root, text="", font=("Helvetica", 14))
+# Esta linha adiciona o Label à janela principal utilizando o método pack,
+# que é um dos métodos de gerenciamento de layout em Tkinter.
 resultado_label.pack(pady=20)
 
 # Inicia o loop da interface gráfica
